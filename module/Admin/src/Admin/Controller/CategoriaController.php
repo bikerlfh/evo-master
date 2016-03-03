d42edbd32569c774e73e3fbec4e6c8d9ade91c51
Luis Fernando Henriquez <bikerlfh@hotmail.com>
2016-03-02 19:15:14 -0500

    Se crean los controladores de los procesos.

---
+++ module/Admin/src/Admin/Controller/CategoriaController.php
@@ -0,0 +1,12 @@
+<?php
+namespace Admin\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+use Admin\Form\FormCategoria;
+use Application\Model\Entity\Categoria;
+
+class CategoriaController extends AbstractActionController
+{
+
+}
