aafec3b7bd55e3282e8867fb397aaf470cba91da
bikerlfh <bikerlfh@hotmail.com>
2016-03-05 15:19:16 -0500

    evo-master

--- module/Application/src/Application/Model/Entity/Departamento.php
+++ module/Application/src/Application/Model/Entity/Departamento.php
@@ -3,7 +3,6 @@ namespace Application\Model\Entity;
 use Zend\Db\TableGateway\AbstractTableGateway;
 use Zend\Db\Adapter\Adapter;
 use Zend\Db\Sql\Sql;
-use Zend\Db\Sql\Select;
 use Zend\Db\ResultSet\ResultSet;
 use Zend\Db\Sql\TableIdentifier;
 use Zend\Db\Sql\Expression;
