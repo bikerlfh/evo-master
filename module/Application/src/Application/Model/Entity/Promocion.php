77e15b4e077158a80fc2655992301a6da1a82baa
bikerlfh <bikerlfh@hotmail.com>
2016-03-27 14:24:56 -0500

    Se crea la clase Promocion

---
+++ module/Application/src/Application/Model/Entity/Promocion.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace Application\Model\Entity;
+use Zend\Db\TableGateway\AbstractTableGateway;
+use Zend\Db\Adapter\Adapter;
+use Zend\Db\Sql\Sql;
+use Zend\Db\ResultSet\ResultSet;
+use Zend\Db\Sql\TableIdentifier;
+use Zend\Db\Sql\Expression;
+
+class Promocion extends AbstractTableGateway
+{
+    
+    private $idPromocion;
+    private $idSaldoInventario;
+    private $valorAnterior;
+    private $valorPromocion;
+    private $fechaDesde;
+    private $fechaHasta;
+    private $estado;
+    private $idUsuarioCreacion;
+    
+    public function __construct(Adapter $adapter = null)
+    {
+        $this->adapter = $adapter;
+        $this->table =  new \Zend\Db\Sql\TableIdentifier('Promocion', 'Producto');
+    }
+    
+    function getIdPromocion() { return $this->idPromocion; }
+    function getIdSaldoInventario() { return $this->idSaldoInventario; }
+    function getValorAnterior() { return $this->valorAnterior; }
+    function getValorPromocion() { return $this->valorPromocion; }
+    function getFechaDesde() { return $this->fechaDesde; }
+    function getFechaHasta() { return $this->fechaHasta; }
+    function getEstado() { return $this->estado; }
+    function getIdUsuarioCreacion() { return $this->idUsuarioCreacion; }
+    function setIdPromocion($idPromocion) { $this->idPromocion = $idPromocion; }
+    function setIdSaldoInventario($idSaldoInventario) { $this->idSaldoInventario = $idSaldoInventario; }
+    function setValorAnterior($valorAnterior) { $this->valorAnterior = $valorAnterior; }
+    function setValorPromocion($valorPromocion) { $this->valorPromocion = $valorPromocion; }
+    function setFechaDesde($fechaDesde) { $this->fechaDesde = $fechaDesde; }
+    function setFechaHasta($fechaHasta) { $this->fechaHasta = $fechaHasta; }
+    function setEstado($estado) { $this->estado = $estado; }
+    function setIdUsuarioCreacion($idUsuarioCreacion) { $this->idUsuarioCreacion = $idUsuarioCreacion; }
+
+    public function guardarPromocion($idSaldoInventario,$valorAnterior,$valorPromocion,$fechaDesde,$fechaHasta,$idUsuarioCreacion)
+    {
+        $datos=array(
+                'idSaldoInventario'=> $idSaldoInventario,
+                'valorAnterior'=> $valorAnterior,
+                'valorPromocion'=> $valorPromocion,
+                'fechaDesde'=> $fechaDesde,
+                'fechaHasta'=> $fechaHasta,
+                'idUsuarioCreacion'=> $idUsuarioCreacion);
+        
+        $result=$this->insert($datos);
+        if($result>0)
+            return true;
+        return false;
+    }
+    
+    public function modificarPromocion($idPromocion,$idSaldoInventario,$valorAnterior,$valorPromocion,$fechaDesde,$fechaHasta,$estado)
+    {
+        $datos=array(
+                'idSaldoInventario'=> $idSaldoInventario,
+                'valorAnterior'=> $valorAnterior,
+                'valorPromocion'=> $valorPromocion,
+                'fechaDesde'=> $fechaDesde,
+                'fechaHasta'=> $fechaHasta,
+                'estado'=> $estado);
+        
+        $result=$this->update($datos,array('idPromocion'=>$idPromocion));
+        if($result>0)
+            return true;
+        return false;
+    }
+    
+    public function eliminarPromocion($idPromocion)
+    {
+        if ($this->delete(array('idPromocion'=>$idPromocion)) > 0)
+            return true;
+        return false;
+    }
+    
+    public function consultarTodoPromocion()
+    {
+        $sql = new Sql($this->adapter);        
+        $select = $sql->select()->
+                         from(array('pro'=> $this->table))->
+                         columns(array('idPromocion','idSaldoInventario','valorAnterior','valorPromocion','fechaDesde','fechaHasta','estado'))->
+                         join(array('s'=> new TableIdentifier("SaldoInventario","Inventario")),
+                                    's.idSaldoInventario = pro.idSaldoInventario')->
+                         join(array("p"=> new TableIdentifier("Producto","Producto")),
+                                    "p.idProducto = s.idProducto",
+                                    array("nombreProducto"=> new Expression("(p.codigo + ' - ' + p.nombre)")));
+        
+        $results = $sql->prepareStatementForSqlObject($select)->execute();
+        $resultsSet = new ResultSet();
+        $resultsSet->initialize($results)->toArray();
+        return   $resultsSet;
+    }
+    
+    public function consultarPromocionPorIdPromocion($idPromocion)
+    {
+        $result=$this->select(array('idPromocion'=>$idPromocion))->current();
+        if($result)
+        {
+            $this->LlenarEntidad($result);
+            return true;
+        }
+        return false;
+    }
+    
+    public function consultarPromocionPorIdSaldoInventario($idSaldoInventario)
+    {
+        $result=$this->select(array('idSaldoInventario'=>$idSaldoInventario))->current();
+        if($result)
+        {
+            $this->LlenarEntidad($result);
+            return true;
+        }
+        return false;
+    }
+    
+    private function LlenarEntidad($result)
+    {
+        $this->idPromocion=$result['idPromocion'];
+        $this->idSaldoInventario=$result['idSaldoInventario'];
+        $this->valorAnterior=$result['valorAnterior'];
+        $this->valorPromocion=$result['valorPromocion'];
+        $this->fechaDesde=$result['fechaDesde'];
+        $this->fechaHasta=$result['fechaHasta'];
+        $this->estado=$result['estado'];
+        $this->idUsuarioCreacion=$result['idUsuarioCreacion'];
+    }
+}
