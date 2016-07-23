d8bb952cf64b6a7116763e41a9b4a5f57ed531c9
bikerlfh <bikerlfh@hotmail.com>
2016-07-22 22:28:55 -0500

    evo-master

---
+++ public/js/registroObjetos.js
@@ -0,0 +1,25 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/*
+ * Registro de tablas
+ * Hecho por Luis Fernando Henriquez Arcinegas
+ * */
+$(document).ready(function() {
+    $('#dtcrudindex').DataTable();
+    $('#dtbuscar').DataTable();
+    /*
+    $("#sistemaoperativo").select2();
+    $("#tipoequipo").select2();
+    $("#marcaequipo").select2();
+    $("#gamaequipo").select2();
+    $("#tipoaccesorio").select2();
+    $("#paquetedato").select2();
+    $("#operador").select2();*/
+    
+    
+} );
+
+
